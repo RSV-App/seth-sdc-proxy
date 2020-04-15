5f92169e6efc0a0dba38fd40b2711729470d2db6
Sethaniel Way <seth@Sethaniels-MacBook-Pro.local>
2020-04-15 10:09:00 -0700

    ready to deploy to ec2

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
+require('newrelic');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const axios = require('axios');
+const { menuServiceAddress } = require('../IP_Updates.js');
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/:id', (req, res) => {
@@ -15,6 +19,14 @@ app.get('/:id', (req, res) => {
   } else {
     res.sendFile('index.html', { root: path.resolve(__dirname, '../public') });
   }
+});
+
+app.get('/api/menu/:id', function(req, res) {
+  let address = menuServiceAddress + '/api/menu/'+ req.params.id;
+  // console.log('id: ', req.params.id);
+  axios.get(address)
+    .then(({ data }) => res.status(200).send(data))
+    .catch((error) => res.status(500).send(error));
 });
 
 app.listen(port, () => console.log(`Master app listening on port ${port}`));
